cb7c7c3541c524a1548397e2b5da0c6828b4de53
Vitor Hugo <vitorzcomputacao@gmail.com>
2021-02-22 22:26:34 -0300

    using pipe operator to sum numbers from csv

---
+++ rocketpay/lib/rocketpay/numbers.ex
@@ -0,0 +1,11 @@
+defmodule Rocketpay.Numbers do
+  def sum_from_file(filename) do
+    "#{filename}.csv"
+    |> File.read()
+    |> handle_file()
+  end
+
+  defp handle_file({:ok, file}), do: file
+  defp handle_file({:error, _reason}), do: {:error, "Invalid file!"}
+
+end
