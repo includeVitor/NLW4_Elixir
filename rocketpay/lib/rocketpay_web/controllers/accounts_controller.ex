e4ff5104352095df7e495db657e8dcc320f3a508
Vitor Hugo <vitorzcomputacao@gmail.com>
2021-02-25 00:25:13 -0300

    create router, facade and controller of Account

---
+++ rocketpay/lib/rocketpay_web/controllers/accounts_controller.ex
@@ -0,0 +1,16 @@
+defmodule RocketpayWeb.AccountsController do
+  use RocketpayWeb, :controller
+
+  alias Rocketpay.Account
+
+  action_fallback RocketpayWeb.FallbackController
+
+  def create(conn, params) do
+    with {:ok, %Account{} = account} <- Rocketpay.deposit(params) do
+      conn
+      |> put_status(:created)
+      |> render("update.json", account: account)
+    end
+  end
+
+end
