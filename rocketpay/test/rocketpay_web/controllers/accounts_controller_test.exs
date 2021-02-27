986f9e197c4067068c5b981f76e4591646d9e062
Vitor Hugo <vitorzcomputacao@gmail.com>
2021-02-26 23:59:59 -0300

    finish up accounts_controller

--- rocketpay/test/rocketpay_web/controllers/accounts_controller_test.exs
+++ rocketpay/test/rocketpay_web/controllers/accounts_controller_test.exs
@@ -35,5 +35,18 @@ defmodule RocketpayWeb.AccountsControllerTest do
                   "message" => "Ballance changed successfully"
                 } = response
       end
+
+      test "when all params are invalid params, returns an error", %{conn: conn, account_id: account_id} do
+        params = %{"value" => "banana"}
+
+        response =
+          conn
+          |> post(Routes.accounts_path(conn, :deposit, account_id, params))
+          |> json_response(:bad_request)
+
+        expected_response = %{"message" => "Invalid deposit value!"}
+
+        assert response == expected_response
+      end
   end
 end
